59614046879bbd240e69fbd2e5b5c144deeef431
Michael Weatherford <michaelweatherford@Michaels-MacBook-Air-6.local>
2025-03-10 17:38:31 +0100

    I LOVE COMMITTING

--- Exercises/Chapter4ReportExercise.R
+++ Exercises/Chapter4ReportExercise.R
@@ -19,3 +19,5 @@ soilcarbonSummary <- soilcarbon %>%
   summarise(mean_ambient_CO2=mean(mean_ambient_CO2),
             mean_elevated_CO2=mean(mean_increased_CO2),
             mean_log_response = mean(log_response))%>%
+  arrange(mean_elevated_CO2)
+  
