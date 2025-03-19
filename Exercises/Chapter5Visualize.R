5bf90214b11281bfdff4d165d319362dda75401d
Michael Weatherford <michaelweatherford@Michaels-MacBook-Air-6.local>
2025-03-19 15:50:50 +0100

    class exercise

--- Exercises/Chapter5Visualize.R
+++ Exercises/Chapter5Visualize.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(readr)
 library(lubridate)
 library(dplyr)
+library(checkthat)
 
 half_hourly_fluxes <- readr::read_csv("data/FLX_CH-Lae_FLUXNET2015_FULLSET_HH_2004-2006_CLEAN.csv")
 
@@ -326,3 +327,13 @@ plotme
 ggsave("./figures/plot_gpp.pdf")
 
 
+half_hourly_fluxes_duped <- half_hourly_fluxes
+half_hourly_fluxes_duped$is_duped <- duplicated(half_hourly_fluxes$GPP_NT_VUT_REF)
+
+daily_fluxes_duped <- half_hourly_fluxes_duped %>%
+  mutate(year = year(ts_start),
+         month = month(ts_start),
+         doy = yday(ts_start))%>%
+  group_by(doy)%>%
+  summarise(GPP_NT_VUT_REF = mean(GPP_NT_VUT_REF), 
+            spur_perc = prop(is_duped))
